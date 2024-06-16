a582405d8ad6fdbfbdec371137c43438c4b64b7e
Ruslan Kazmiryk <callogan217@gmail.com>
2024-06-16 17:32:13 +0300

    Establish the idea of the book on model level

--- book/models.py
+++ book/models.py
@@ -1,3 +1,21 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    COVER_CHOICES = [("H", "HARD"), ("S", "SOFT")]
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(max_length=1, choices=COVER_CHOICES)
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(
+        max_digits=5, decimal_places=2, validators=[MinValueValidator(0)]
+    )
+
+    class Meta:
+        ordering = ("title",)
+
+    def __str__(self):
+        return self.title
+    
