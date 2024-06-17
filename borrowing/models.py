b4ac43dd412b171082b16c1ab8122abeb032f864
Ruslan Kazmiryk <callogan217@gmail.com>
2024-06-17 12:06:34 +0300

    Embody the vision of the borrowing on model level

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+from book.models import Book
+from library_service import settings
+
+
+class Borrowing(models.Model):
+
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name="borrowings"
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="borrowings"
+    )
+
+    class Meta:
+        ordering = ("borrow_date",)
+
+    def __str__(self):
+        return f"{self.book.title} borrowed by {self.user}"
