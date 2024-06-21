8ed5de241166ae431664be971f149849f481119a
Ruslan Kazmiryk <callogan217@gmail.com>
2024-06-21 22:18:43 +0300

    Create the view for borrowing app

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from borrowing.models import Borrowing
+from borrowing.serializers import (
+    BorrowingSerializer,
+    BorrowingCreateSerializer,
+    BorrowingListSerializer,
+    BorrowingDetailSerializer,
+)
+
+
+class BorrowingViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    GenericViewSet
+):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    @staticmethod
+    def _params_to_ints(qs):
+        return [int(str_id) for str_id in qs.split(",")]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.action in ("list", "retrieve") and not self.request.user.is_staff:
+            return queryset.filter(user_id=self.request.user.id)
+
+        user = self.request.query_params.get("user")
+        is_active = self.request.query_params.get("is_active", "").lower()
+
+        if is_active == "true":
+            queryset = queryset.filter(
+                actual_return_date__isnull=True
+            )
+
+        if is_active == "false":
+            queryset = queryset.filter(
+                actual_return_date__isnull=False
+            )
+
+        if self.request.user.is_staff:
+            if user:
+                user_id = self._params_to_ints(user)
+                queryset = queryset.filter(user_id__in=user_id)
+
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BorrowingListSerializer
+        if self.action == "retrieve":
+            return BorrowingDetailSerializer
+        if self.action == "create":
+            return BorrowingCreateSerializer
+        return self.serializer_class
