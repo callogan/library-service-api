77f4b65cecb4bb75177698f8d91a525dc19917fd
Ruslan Kazmiryk <callogan217@gmail.com>
2024-06-25 13:47:02 +0300

    Add permissions for borrowing app

--- borrowing/views.py
+++ borrowing/views.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from django.db import transaction
 from rest_framework import mixins
 from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
@@ -26,6 +27,7 @@ class BorrowingViewSet(
 ):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+    permission_classes = (IsAuthenticated,)
 
     def create(self, request, *args, **kwargs):
         response = super().create(request, *args, **kwargs)
