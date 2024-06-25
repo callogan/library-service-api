368b0fdd96eba237142ea47af8a8007ca42e0407
Ruslan Kazmiryk <callogan217@gmail.com>
2024-06-25 13:53:24 +0300

    Effectuate the idea of the payment on model level

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,38 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+from borrowing.models import Borrowing
+
+
+class Payment(models.Model):
+    STATUS_CHOICE = [
+        ("PENDING", "Pending"),
+        ("PAID", "Paid")
+    ]
+
+    status = models.CharField(
+        max_length=7,
+        choices=STATUS_CHOICE,
+        default="PENDING"
+    )
+    borrowing = models.ForeignKey(
+        Borrowing,
+        on_delete=models.CASCADE,
+        related_name="payments"
+    )
+    session_url = models.TextField(
+        max_length=450,
+        null=True,
+        blank=True,
+        unique=True
+    )
+    session_id = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+        unique=True
+    )
+    money_to_pay = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
+
+    def __str__(self) -> str:
+        return f"{self.status} ({self.money_to_pay}USD)"
